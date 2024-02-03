dda8c9db9d33da7ba2021367738ae5ff34acd7a4
chandu7717 <chanduudgir>
2024-02-03 23:33:06 +0530

    ss

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import  "./index.css"
+
+const App = () => {
+  return (
+    <div id='main'>
+       <nav>
+         <div id="module">
+          <ol>
+            <li>
+              <a href="">
+               Home
+              </a>
+            </li>
+            <li>
+              <a href="">
+               About
+              </a>
+            </li>
+            <li>
+              <a href="">
+               Projects
+              </a>
+            </li>
+            <li>
+              <a href="">
+               Resume
+              </a>
+            </li>
+            <li>
+              <a href="">
+               Contact
+              </a>
+            </li>
+          </ol>
+         </div>
+       </nav>
+    </div>
+  )
+}
+
+export default App
